73fdcd627836bfaab91471cdeded51ecb8ddea7f
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2026-02-13 08:47:44 -0700

    Update mask to pass by value.

--- config_src/infra/TIM/MOM_coms_infra.F90
+++ config_src/infra/TIM/MOM_coms_infra.F90
@@ -43,10 +43,10 @@ end interface field_chksum
 interface tim_chksum
   function tim_chksum_r8_1d(field_ptr, field_size, mask_ptr) bind(c, name="tim_chksum_r8_1d")
     import c_ptr, c_int64_t, c_size_t
-    integer(c_int64_t)                       :: tim_chksum_r8_1d
-    type(c_ptr),            value,    intent(in) :: field_ptr
-    integer(c_size_t),      value,    intent(in) :: field_size
-    type(c_ptr),            optional, intent(in) :: mask_ptr
+    integer(c_int64_t)                        :: tim_chksum_r8_1d
+    type(c_ptr),            value, intent(in) :: field_ptr
+    integer(c_size_t),      value, intent(in) :: field_size
+    type(c_ptr),            value, intent(in) :: mask_ptr
   end function tim_chksum_r8_1d
 end interface
 
