7fdfa90ed44818f3a7ff4f22af0fbdfa7110e675
johnmauff <dennis@ucar.edu>
2026-03-28 09:41:43 -0600

    Added shim layer to redirect to C++ code.

--- src/core/MOM_continuity_PPM.F90
+++ src/core/MOM_continuity_PPM.F90
@@ -14,6 +14,10 @@ use MOM_unit_scaling, only : unit_scale_type
 use MOM_variables, only : BT_cont_type, porous_barrier_type
 use MOM_verticalGrid, only : verticalGrid_type
 
+! moved the definition of PPM_limit_pos and PPM_limit_cw84 to another module
+use MOM_continuity_PPM_shim, only : PPM_limit_pos => PPM_limit_pos_shim
+use MOM_continuity_PPM_shim, only : PPM_limit_cw84 => PPM_limit_cw84_shim
+
 implicit none ; private
 
 #include <MOM_memory.h>
@@ -2575,91 +2579,6 @@ subroutine PPM_reconstruction_y(h_in, h_S, h_N, G, LB, h_min, monotonic, simple_
 
   return
 end subroutine PPM_reconstruction_y
-
-!> This subroutine limits the left/right edge values of the PPM reconstruction
-!! to give a reconstruction that is positive-definite.  Here this is
-!! reinterpreted as giving a constant thickness if the mean thickness is less
-!! than h_min, with a minimum of h_min otherwise.
-subroutine PPM_limit_pos(h_in, h_L, h_R, h_min, G, iis, iie, jis, jie)
-  type(ocean_grid_type),             intent(in)  :: G    !< Ocean's grid structure.
-  real, dimension(SZI_(G),SZJ_(G)),  intent(in)  :: h_in !< Layer thickness [H ~> m or kg m-2].
-  real, dimension(SZI_(G),SZJ_(G)),  intent(inout) :: h_L !< Left thickness in the reconstruction [H ~> m or kg m-2].
-  real, dimension(SZI_(G),SZJ_(G)),  intent(inout) :: h_R !< Right thickness in the reconstruction [H ~> m or kg m-2].
-  real,                              intent(in)  :: h_min !< The minimum thickness
-                    !! that can be obtained by a concave parabolic fit [H ~> m or kg m-2]
-  integer,                           intent(in)  :: iis      !< Start of i index range.
-  integer,                           intent(in)  :: iie      !< End of i index range.
-  integer,                           intent(in)  :: jis      !< Start of j index range.
-  integer,                           intent(in)  :: jie      !< End of j index range.
-
-! Local variables
-  real    :: curv  ! The grid-normalized curvature of the three thicknesses  [H ~> m or kg m-2]
-  real    :: dh    ! The difference between the edge thicknesses             [H ~> m or kg m-2]
-  real    :: scale ! A scaling factor to reduce the curvature of the fit               [nondim]
-  integer :: i,j
-
-  do j=jis,jie ; do i=iis,iie
-    ! This limiter prevents undershooting minima within the domain with
-    ! values less than h_min.
-    curv = 3.0*((h_L(i,j) + h_R(i,j)) - 2.0*h_in(i,j))
-    if (curv > 0.0) then ! Only minima are limited.
-      dh = h_R(i,j) - h_L(i,j)
-      if (abs(dh) < curv) then ! The parabola's minimum is within the cell.
-        if (h_in(i,j) <= h_min) then
-          h_L(i,j) = h_in(i,j) ; h_R(i,j) = h_in(i,j)
-        elseif (12.0*curv*(h_in(i,j) - h_min) < (curv**2 + 3.0*dh**2)) then
-          ! The minimum value is h_in - (curv^2 + 3*dh^2)/(12*curv), and must
-          ! be limited in this case.  0 < scale < 1.
-          scale = 12.0*curv*(h_in(i,j) - h_min) / (curv**2 + 3.0*dh**2)
-          h_L(i,j) = h_in(i,j) + scale*(h_L(i,j) - h_in(i,j))
-          h_R(i,j) = h_in(i,j) + scale*(h_R(i,j) - h_in(i,j))
-        endif
-      endif
-    endif
-  enddo ; enddo
-
-end subroutine PPM_limit_pos
-
-!> This subroutine limits the left/right edge values of the PPM reconstruction
-!! according to the monotonic prescription of Colella and Woodward, 1984.
-subroutine PPM_limit_CW84(h_in, h_L, h_R, G, iis, iie, jis, jie)
-  type(ocean_grid_type),             intent(in)  :: G     !< Ocean's grid structure.
-  real, dimension(SZI_(G),SZJ_(G)),  intent(in)  :: h_in  !< Layer thickness [H ~> m or kg m-2].
-  real, dimension(SZI_(G),SZJ_(G)),  intent(inout) :: h_L !< Left thickness in the reconstruction,
-                                                          !! [H ~> m or kg m-2].
-  real, dimension(SZI_(G),SZJ_(G)),  intent(inout) :: h_R !< Right thickness in the reconstruction,
-                                                          !! [H ~> m or kg m-2].
-  integer,                           intent(in)  :: iis   !< Start of i index range.
-  integer,                           intent(in)  :: iie   !< End of i index range.
-  integer,                           intent(in)  :: jis   !< Start of j index range.
-  integer,                           intent(in)  :: jie   !< End of j index range.
-
-  ! Local variables
-  real    :: h_i      ! A copy of the cell-average layer thickness                [H ~> m or kg m-2]
-  real    :: RLdiff   ! The difference between the input edge values              [H ~> m or kg m-2]
-  real    :: RLdiff2  ! The squared difference between the input edge values   [H2 ~> m2 or kg2 m-4]
-  real    :: RLmean   ! The average of the input edge thicknesses                 [H ~> m or kg m-2]
-  real    :: FunFac   ! A curious product of the thickness slope and curvature [H2 ~> m2 or kg2 m-4]
-  integer :: i, j
-
-  do j=jis,jie ; do i=iis,iie
-    ! This limiter monotonizes the parabola following
-    ! Colella and Woodward, 1984, Eq. 1.10
-    h_i = h_in(i,j)
-    if ( ( h_R(i,j) - h_i ) * ( h_i - h_L(i,j) ) <= 0. ) then
-      h_L(i,j) = h_i ; h_R(i,j) = h_i
-    else
-      RLdiff = h_R(i,j) - h_L(i,j)            ! Difference of edge values
-      RLmean = 0.5 * ( h_R(i,j) + h_L(i,j) )  ! Mean of edge values
-      FunFac = 6. * RLdiff * ( h_i - RLmean ) ! Some funny factor
-      RLdiff2 = RLdiff * RLdiff               ! Square of difference
-      if ( FunFac >  RLdiff2 ) h_L(i,j) = 3. * h_i - 2. * h_R(i,j)
-      if ( FunFac < -RLdiff2 ) h_R(i,j) = 3. * h_i - 2. * h_L(i,j)
-    endif
-  enddo ; enddo
-
-  return
-end subroutine PPM_limit_CW84
 
 !> Return the maximum ratio of a/b or maxrat.
 function ratio_max(a, b, maxrat) result(ratio)
